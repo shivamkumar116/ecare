9cc77008cb12f7c991d77b0d8d2bf90cde053849
shivam <shivamkumar116@gmail.com>
2020-03-30 23:20:22 +0530

    Done ForgotPassword, Register

--- ecare/Controllers/HomeController.cs
+++ ecare/Controllers/HomeController.cs
@@ -11,62 +11,162 @@ using System.Web.Configuration;
 namespace ecare.Controllers
 {
     public class HomeController : Controller
-
-
     {
 
-        string cs = WebConfigurationManager.ConnectionStrings["dbECare"].ConnectionString;
+        private readonly string cs = WebConfigurationManager.ConnectionStrings["dbECare"].ConnectionString;
 
         public ActionResult Index()
         {
-
-
             return View();
         }
+
         public ActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(Login objLogin)
+        public ActionResult Login(Credentials objUser)
         {
-            SqlConnection con = new SqlConnection(cs);
-            try
+            if (ModelState.IsValid)
             {
-
-                con.Open();
-                SqlCommand cmd = new SqlCommand("pValidateUser", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                cmd.Parameters.AddWithValue("@Email", objLogin.Email);
-                cmd.Parameters.AddWithValue("@Password", objLogin.Password);
-                DataTable dt = new DataTable();
-                SqlDataAdapter adp = new SqlDataAdapter(cmd);
-                adp.Fill(dt);
-                if (dt.Rows.Count > 0)
-                {
-                    Session["Email"] = dt.Rows[0]["Email"].ToString();
-
-                    RedirectToAction("Index");
-                }
-
-                else
+                SqlConnection con = new SqlConnection(cs);
+                try
                 {
 
-                    RedirectToAction("Login");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("pValidateUser", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    cmd.Parameters.AddWithValue("@Email", objUser.Email);
+                    cmd.Parameters.AddWithValue("@Password", objUser.Password);
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    adp.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        Session["Email"] = dt.Rows[0]["Email"].ToString();
+                        Session["Name"] = dt.Rows[0]["Name"].ToString();
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+
+                        return RedirectToAction("Login", "Home");
+                    }
+                }
+                catch (SqlException ex)
+                {
+
 
                 }
-            }
-            catch (SqlException ex)
-            {
-            }
-            finally
-            {
-                con.Close();
+                finally
+                {
+                    con.Close();
+                    ModelState.Clear();
+                }
             }
 
-            return View(objLogin);
+            return View();
         }
+
+
+
+
+        public ActionResult Register()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Register(User objUser)
+        {
+
+            if (ModelState.IsValid)
+            {
+                SqlConnection con = new SqlConnection(cs);
+                try
+                {
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("pInsertRegisterMaster", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("@Name", objUser.Name);
+                    cmd.Parameters.AddWithValue("@Email", objUser.Email);
+                    cmd.Parameters.AddWithValue("@Password", objUser.Password);
+                    cmd.ExecuteNonQuery();
+                    ViewBag.Message = objUser.Name + " is Registered Successfully";
+                    ModelState.Clear();
+                    return RedirectToAction("Register", "Home");
+
+                }
+
+                catch (SqlException ex)
+                {
+
+
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            return View();
+
+        }
+
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(ForgotPassword objForgotPassword)
+        {
+            if (ModelState.IsValid)
+            {
+                SqlConnection con = new SqlConnection(cs);
+                try
+                {
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Select Password from RegisterMaster where Email=@email", con);
+                    cmd.Parameters.AddWithValue("@email", objForgotPassword.Email);
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    adp.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        String password = dt.Rows[0]["Password"].ToString();
+                        ViewBag.Message = password;
+                        ModelState.Clear();
+                        //Code to send the email
+
+                    }
+                    else
+                    {
+                        ViewBag.Message = "No record Found!";
+                        ModelState.Clear();
+                    }
+
+
+                  
+                }
+                catch (Exception e) { }
+                finally { con.Close(); }
+            }
+       
+            return View();
+        }
+
+
     }
 }
---
+++ ecare/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ecare.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter Confirm Password")]
+        [Compare("Password", ErrorMessage = "password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+
+
+
+    }
+    public class Credentials
+    {
+        [Required(ErrorMessage = "Please enter Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+    }
+
+    public class ForgotPassword
+    {
+        [Required(ErrorMessage = "Please enter Email")]
+        public string Email { get; set; }
+    }
+}
